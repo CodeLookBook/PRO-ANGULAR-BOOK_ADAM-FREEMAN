bd7fe09d7126349e700022e5d2fbceb36c422569
Евгений Мирошниченко <CodeLookBook@gmail.com>
2018-04-14 19:31:00 +0300

    'counter' directive was added to the 'store' functional module.

--- SportsStore/src/app/store/store.module.ts
+++ SportsStore/src/app/store/store.module.ts
@@ -1,13 +1,20 @@
-import { ModelModule } from "../model/model.module";
-import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
-import { NgModule } from "@angular/core";
-import { StoreComponent } from "./store.component";
-
+import { ModelModule      } from "../model/model.module";
+import { BrowserModule    } from "@angular/platform-browser";
+import { FormsModule      } from "@angular/forms";
+import { NgModule         } from "@angular/core";
+import { StoreComponent   } from "./store.component";
+import { CounterDirective } from "./counter.directive";
 
 @NgModule({
-    imports: [ModelModule, BrowserModule, FormsModule],
-    declarations: [StoreComponent],
+    imports: [
+        ModelModule,
+        BrowserModule,
+        FormsModule
+    ],
+    declarations: [
+        StoreComponent,
+        CounterDirective
+    ],
     exports: [StoreComponent]
 })
 export class StoreModule { }
