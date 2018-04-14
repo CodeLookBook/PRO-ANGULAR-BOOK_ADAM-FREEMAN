43bad70791d706bd9b60b35ecbfbb828751fad2f
Евгений Мирошниченко <CodeLookBook@gmail.com>
2018-04-14 17:34:15 +0300

    Store сomponent 'products' property was modified. It returns fixed number of products on the page now. Also 'productsPerPage', 'selectedPage' properties and 'changePage()', 'changePageSize()', 'pageNumber()' methods were added to the component.

--- SportsStore/src/app/store/store.component.ts
+++ SportsStore/src/app/store/store.component.ts
@@ -1,5 +1,5 @@
 import { Component         } from "@angular/core";
-import { Product           } from "../model/product.model"; 
+import { Product           } from "../model/product.model";
 import { ProductRepository } from "../model/product.repository";
 
 @Component({
@@ -9,11 +9,17 @@ import { ProductRepository } from "../model/product.repository";
 })
 export class StoreComponent {
     public selectedCategory : string = null;
+    public productsPerPage  = 4;
+    public selectedPage     = 1;
 
     constructor(private repository : ProductRepository) { }
 
     get products(): Product[] {
-        return this.repository.getProducts(this.selectedCategory);
+        const pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+
+        return this.repository
+            .getProducts(this.selectedCategory)
+            .slice(pageIndex, pageIndex + this.productsPerPage);
     }
 
     get categories(): string[] {
@@ -22,5 +28,20 @@ export class StoreComponent {
 
     changeCategory(newCategory? : string) {
         this.selectedCategory = newCategory;
+    }
+
+    changePage(newPage : number) {
+        this.selectedPage = newPage;
+    }
+
+    changePageSize(newSize : number) {
+        this.productsPerPage = Number(newSize);
+        this.changePage(1);
+    }
+
+    get pageNumber(): number[] {
+        return Array(Math.ceil(this.repository
+            .getProducts(this.selectedCategory).length / this.productsPerPage))
+                .fill(0).map((x, i) => i + 1);
     }
 }
