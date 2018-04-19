a780f488590ccae83750c6f1c22910b1ccd4117c
Евгений Мирошниченко <CodeLookBook@gmail.com>
2018-04-19 21:07:51 +0300

    Updated => admin.module.ts: 1. AuthGuard class was imported to the module file. 2. AuthGuard class was registerd as module provider; 3. AuthGuard class was adeded to the path "/main";

--- SportsStore/src/app/admin/admin.module.ts
+++ SportsStore/src/app/admin/admin.module.ts
@@ -4,10 +4,11 @@ import { AdminComponent } from "./admin.component";
 import { FormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { AuthGuard } from "./auth.guard";
 
 const routing = RouterModule.forChild([
     { path: "auth", component: AuthComponent  },
-    { path: "main", component: AdminComponent },
+    { path: "main", component: AdminComponent, canActivate: [AuthGuard] },
     { path: "**"  , redirectTo: "auth" }
 ]);
 
@@ -16,6 +17,9 @@ const routing = RouterModule.forChild([
         CommonModule,
         FormsModule,
         routing
+    ],
+    providers: [
+        AuthGuard
     ],
     declarations: [
         AuthComponent,
