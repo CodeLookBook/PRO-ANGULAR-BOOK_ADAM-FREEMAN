da2628b5ee49228aa356abe93d89663412690645
Евгений Мирошниченко <CodeLookBook@gmail.com>
2018-04-19 21:02:50 +0300

    Created => auth.guared.ts: 1. AuthGuard class was created; 2. Constructor was created; 3. canActivate() method was created;

---
+++ SportsStore/src/app/admin/auth.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable } from "@angular/core";
+import { AuthService } from "../model/auth.service";
+import { Router, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
+
+/**
+ * Не допускает прямых переходов к функциям администрирования.
+ */
+
+ Injectable();
+ export class AuthGuard {
+    constructor(
+        private router : Router,
+        private auth   : AuthService
+    ) { }
+
+    canActivate(
+        route : ActivatedRouteSnapshot,
+        state : RouterStateSnapshot
+    ): boolean {
+
+        let canActivate = false;
+
+        if (!this.auth.authenticated) {
+            this.router.navigateByUrl("/admin/auth");
+            canActivate = false;
+        } else {
+            canActivate = true;
+        }
+
+        return canActivate;
+    }
+ }
