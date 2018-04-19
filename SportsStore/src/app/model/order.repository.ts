6658827a8b2ebc9ed0adc539e0df753dc04cceb4
Евгений Мирошниченко <CodeLookBook@gmail.com>
2018-04-19 22:29:39 +0300

    Created => order.repository.ts: 1. 'loaded' property was created; 2. loadOrders() - method was created; 3. updateOrder() - method was created; 4. deleteOrder() - method was created;

--- SportsStore/src/app/model/order.repository.ts
+++ SportsStore/src/app/model/order.repository.ts
@@ -2,18 +2,53 @@ import { Injectable       } from "@angular/core";
 import { StaticDataSource } from "./static.datasource";
 import { Order            } from "./order.model";
 import { Observable       } from "rxjs/Observable";
+import { RestDataSource } from "./rest.datasource";
 
 @Injectable()
 export class OrderRepository {
     private orders : Order[] = [];
+    private loaded = false;
 
-    constructor(private dataSource : StaticDataSource) { }
+    constructor(private dataSource : RestDataSource) { }
+
+    loadORders() {
+        this.loaded = true;
+        this.dataSource
+            .getOrders()
+            .subscribe(orders => this.orders = orders);
+    }
 
     getOrders(): Order[] {
+
+        if (!this.loaded) {
+            this.loadORders();
+        }
+
         return this.orders;
+    }
+
+    updateOrder(order : Order) {
+        this.dataSource
+            .updateOrder(order)
+            .subscribe(o => {
+                this.orders
+                    .splice(this.orders.findIndex(or => {
+                        return or.id === or.id;
+                    }), 1, o);
+            });
     }
 
     saveOrder(order : Order): Observable<Order> {
         return this.dataSource.saveOrder(order);
+    }
+
+    deleteOrder(id : number) {
+        this.dataSource
+            .deleteOrder(id)
+            .subscribe(order => {
+                this.orders.splice(this.orders.findIndex(o => {
+                    return id === o.id;
+                }));
+            });
     }
 }
