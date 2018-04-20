7fe85c4df966e5c5e322a9484a2d39bb3764f1e9
Евгений Мирошниченко <CodeLookBook@gmail.com>
2018-04-20 16:53:17 +0300

    Created => product-editor.component.ts: 1. Constructor was created; 2. save() - method was created;

--- SportsStore/src/app/admin/product-editor.component.ts
+++ SportsStore/src/app/admin/product-editor.component.ts
@@ -1,8 +1,44 @@
 import { Component } from "@angular/core";
+import { Product } from "../model/product.model";
+import { ProductRepository } from "../model/product.repository";
+import { Router, ActivatedRoute } from "@angular/router";
+import { NgForm } from "@angular/forms";
 
 @Component({
-    template: `<div class="bg-warning p-a-1">
-                    <h3>Product Editor Placeholder</h3>
-                </div>`
+    moduleId: module.id,
+    templateUrl: "product-editor.component.html"
 })
-export class ProductEditorComponent { }
+export class ProductEditorComponent {
+    editing = false;
+    product = new Product();
+
+    /**
+     * @param activeRoute -  используеться для анализа
+     * активизированного маршрута
+     */
+    constructor(
+        private repository : ProductRepository,
+        private router     : Router,
+        activeRoute        : ActivatedRoute) {
+
+        this.editing = activeRoute.snapshot
+            .params["mode"] === "edit";
+
+        if (this.editing) {
+            Object.assign(
+                this.product, this.repository.getProduct(
+                    activeRoute.snapshot
+                        .params["id"]
+                )
+            );
+        }
+    }
+
+    /**
+     * Сохраняет изменения, внесенные пользователем.
+     */
+    save(form : NgForm) {
+        this.repository.saveProduct(this.product);
+        this.router.navigateByUrl("/admin/main/products");
+    }
+ }
