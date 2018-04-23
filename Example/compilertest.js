15761b2273090e0187cc7fc88d431d0ef0f5c8c4
Евгений Мирошниченко <CodeLookBook@gmail.com>
2018-04-23 19:54:13 +0300

    -

---
+++ Example/compilertest.js
@@ -0,0 +1,10 @@
+"use strict";
+// Test file for the TypeScript compiler.
+Object.defineProperty(exports, "__esModule", { value: true });
+var Test = /** @class */ (function () {
+    function Test(message) {
+        this.message = message;
+    }
+    return Test;
+}());
+exports.Test = Test;
