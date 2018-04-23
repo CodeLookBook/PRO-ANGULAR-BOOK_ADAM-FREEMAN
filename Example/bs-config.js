e790cdfebb77c18437f47c5815524fe0e83f9599
Евгений Мирошниченко <CodeLookBook@gmail.com>
2018-04-23 20:18:50 +0300

    bs-config.js: 1. BrowserSync was configurated (server-lite npm package).

---
+++ Example/bs-config.js
@@ -0,0 +1,14 @@
+/**
+ * BrowserSync обладает широким набором функций, которые могут
+ *  настраиваться в файле bs-config.js. За дополнительной 
+ * информацией обращайтесь по адресу 
+ * https://www.browsersync.io/docs/options
+ */
+
+module.exports = {
+    ghostMode: false, 
+    reloadDelay: 1000,
+    reloadDebounce: 1000, 
+    injectChanges: false, 
+    minify: false
+}
